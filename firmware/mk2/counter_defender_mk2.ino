5d6f2cba2872820eb34a28bc7d742ff623d5696f
Sean Caulfield <sean@yak.net>
2017-04-09 13:53:52 -0400

    rearrange for multiple version of the firmware

---
+++ firmware/mk2/counter_defender_mk2.ino
@@ -0,0 +1,18 @@
+/*
+ * counter_defender.ino
+ *
+ * Keep my goddamn cats off the goddamn countertops.
+ *
+ * Author: Sean Caulfield <sean@yak.net>
+ * License: GPL v2.0
+ *
+ */
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+void setup() {
+}
+
+void loop() {
+}
